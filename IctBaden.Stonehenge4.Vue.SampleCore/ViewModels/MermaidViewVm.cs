67f6b0875baa359b8f3f76b29faa56c7db639b28
Frank Pfattheicher <fpf@ict-baden.de>
2024-04-15 16:42:30 +0200

    Refactoring

--- IctBaden.Stonehenge4.Vue.SampleCore/ViewModels/MermaidViewVm.cs
+++ IctBaden.Stonehenge4.Vue.SampleCore/ViewModels/MermaidViewVm.cs
@@ -22,25 +22,29 @@ public class MermaidViewVm : ActiveViewModel
         switch (graph)
         {
             case 1:
-                MermaidGraph = @"graph TD;  Start --> Stop;";
-                MermaidGraph2 = @"graph TD;  Begin --> End;";
+                MermaidGraph = "graph TD;  Start --> Stop;";
+                MermaidGraph2 = "graph TD;  Begin --> End;";
                 break;
             case 2:
-                MermaidGraph = @"graph LR;
-    A--> B & C & D;
-    B--> A & E;
-    C--> A & E;
-    D--> A & E;
-    E--> B & C & D;";
-                MermaidGraph2 = @"sequenceDiagram
-    Alice ->> Bob: Hello Bob, how are you?
-    Bob-->>John: How about you John?
-    Bob--x Alice: I am good thanks!
-    Bob-x John: I am good thanks!
-    Note right of John: Bob thinks a long<br/>long time, so long<br/>that the text does<br/>not fit on a row.
-
-    Bob-->Alice: Checking with John...
-    Alice->John: Yes... John, how are you?";
+                MermaidGraph = """
+                               graph LR;
+                                   A--> B & C & D;
+                                   B--> A & E;
+                                   C--> A & E;
+                                   D--> A & E;
+                                   E--> B & C & D;
+                               """;
+                MermaidGraph2 = """
+                                sequenceDiagram
+                                    Alice ->> Bob: Hello Bob, how are you?
+                                    Bob-->>John: How about you John?
+                                    Bob--x Alice: I am good thanks!
+                                    Bob-x John: I am good thanks!
+                                    Note right of John: Bob thinks a long<br/>long time, so long<br/>that the text does<br/>not fit on a row.
+                                
+                                    Bob-->Alice: Checking with John...
+                                    Alice->John: Yes... John, how are you?
+                                """;
                 break;
         }
     }
