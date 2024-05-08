140ec223bb8deaf61a315da94c25cdab357937f8
Frank Pfattheicher <fpf@ict-baden.de>
2024-05-08 08:10:00 +0200

    Info page updated

--- IctBaden.Stonehenge4.Vue.SampleCore/ViewModels/InfoVm.cs
+++ IctBaden.Stonehenge4.Vue.SampleCore/ViewModels/InfoVm.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
+using IctBaden.Framework.AppUtils;
 using IctBaden.Stonehenge.Core;
 using IctBaden.Stonehenge.ViewModel;
 
@@ -12,6 +14,10 @@ public class InfoVm : ActiveViewModel
 {
     public string AppReleaseDate { get; private set; } = string.Empty;
 
+    public string RuntimeDirectory { get; private set; } = string.Empty;
+    public bool IsSelfHosted { get; private set; }
+    public string ClrVersion { get; private set; } = string.Empty;
+    
     public InfoVm(AppSession session) : base(session)
     {
     }
@@ -20,5 +26,9 @@ public class InfoVm : ActiveViewModel
     public override void OnLoad()
     {
         AppReleaseDate = File.GetCreationTime(Environment.ProcessPath!).Date.ToString("d");
+
+        RuntimeDirectory = RuntimeEnvironment.GetRuntimeDirectory();
+        IsSelfHosted = FrameworkInfo.IsSelfHosted;
+        ClrVersion = RuntimeEnvironment.GetSystemVersion();
     }
 }
