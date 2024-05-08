7dc742f572c8e84f95b2ef236f3ebd8530ce10e5
Frank Pfattheicher <fpf@ict-baden.de>
2024-05-08 12:12:15 +0200

    Nuget net8.0

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@
 version=$(grep -oP -m 1 '\* \K[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*' docs/ReleaseNotes.md)
 echo Version=$version
 
-dotnet publish -r linux-x64 -c Release -f net6.0 -p:AssemblyVersion=$version --self-contained
+dotnet publish -r linux-x64 -c Release -f net8.0 -p:AssemblyVersion=$version --self-contained
 
