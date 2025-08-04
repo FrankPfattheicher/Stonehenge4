533a419b52ba444b75f00b847ae10fe7809923ad
Frank Pfattheicher <fpf@ict-baden.de>
2025-08-04 11:32:06 +0200

    Added st-passwd

--- IctBaden.Stonehenge4/Core/Passwords.cs
+++ IctBaden.Stonehenge4/Core/Passwords.cs
@@ -1,5 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using CryptSharp;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace IctBaden.Stonehenge.Core;
 
@@ -16,25 +20,99 @@ public class Passwords
         _fileName = fileName;
     }
 
-    // ReSharper disable once UnusedMember.Global
-    public bool IsValid(string user, string password)
+    public IList<string> GetUsers()
     {
-        if (string.IsNullOrEmpty(password)) return false;
-
-        if(!File.Exists(_fileName)) return false;
-        
+        var users = new List<string>();
         var lines = File.ReadAllLines(_fileName);
         // ReSharper disable once LoopCanBeConvertedToQuery
         foreach (var line in lines)
         {
             var userPw = line.Split(':');
             if (userPw.Length != 2) continue;
-            if (!string.Equals(userPw[0], user, System.StringComparison.Ordinal)) continue;
 
-            return Crypter.CheckPassword(password, userPw[1]);
+            users.Add(userPw[0]);
+        }
+        return users;
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public bool IsValid(string user, string password)
+    {
+        if (string.IsNullOrEmpty(password)) return false;
+
+        var lines = File.ReadAllLines(_fileName);
+        foreach (var line in lines)
+        {
+            var userPw = line.Split(':');
+            if (userPw.Length != 2) continue;
+            if (!string.Equals(userPw[0], user, StringComparison.Ordinal)) continue;
+
+            var pw = userPw[1].Split(['$'], StringSplitOptions.RemoveEmptyEntries);
+            if (pw.Length != 3) continue;
+
+            var method = pw[0];
+            var salt = Convert.FromBase64String(pw[1]);
+            var hash = pw[2];
+
+            var pwBytes = Encoding.ASCII.GetBytes(password);
+            var pwSalt = new byte[pwBytes.Length + salt.Length];
+            Array.Copy(pwBytes, pwSalt, pwBytes.Length);
+            Array.Copy(salt, 0, pwSalt, pwBytes.Length, salt.Length);
+
+            switch (method)
+            {
+                case "5":
+                    return string.Equals(hash, Convert.ToBase64String(SHA256.HashData(pwSalt)), StringComparison.Ordinal);
+                case "6":
+                    return string.Equals(hash, Convert.ToBase64String(SHA512.HashData(pwSalt)), StringComparison.Ordinal);
+                default:
+                    Trace.TraceError($"Passwords: Unsupported hashing method {method}");
+                    break;
+            }
         }
 
         return false;
+    }
+
+    private List<string> LinesWithoutUser(string user)
+    {
+        var newLines = new List<string>();
+        var lines = File.ReadAllLines(_fileName);
+        foreach (var line in lines)
+        {
+            var userPw = line.Split(':');
+            if (userPw.Length != 2) continue;
+            if (!string.Equals(userPw[0], user, StringComparison.Ordinal))
+            {
+                newLines.Add(line);
+            }
+        }
+        return newLines;
+    }
+
+    public void RemoveUser(string user)
+    {
+        var newLines = LinesWithoutUser(user);
+        File.WriteAllLines(_fileName, newLines);
+    }
+
+    public void SetPassword(string user, string password)
+    {
+        var newLines = LinesWithoutUser(user);
+
+        var rnd = new Random();
+        var salt = new byte[12];
+        rnd.NextBytes(salt);
+        var pwBytes = Encoding.ASCII.GetBytes(password);
+        var pwSalt = new byte[pwBytes.Length + salt.Length];
+        Array.Copy(pwBytes, pwSalt, pwBytes.Length);
+        Array.Copy(salt, 0, pwSalt, pwBytes.Length, salt.Length);
+        var hash = Convert.ToBase64String(SHA512.HashData(pwSalt));
+
+        var newLine = $"{user}:$6${Convert.ToBase64String(salt)}${hash}";
+        newLines.Add(newLine);
+
+        File.WriteAllLines(_fileName, newLines);
     }
 
 }
