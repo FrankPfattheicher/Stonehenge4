775cbe60fcbe800d02514ca21a2f918cec21bc50
Frank Pfattheicher <fpf@ict-baden.de>
2026-02-28 16:42:24 +0100

    WIP: st-splitter

---
+++ IctBaden.Stonehenge4.Forms/app/st-splitter.js
@@ -0,0 +1,20 @@
+
+mounted: function() {
+
+    const splitter = this.$el;
+
+    splitter.onmousedown = function(e) {
+
+        debugger;
+
+    }
+    splitter.onmousemove = function(e) {
+        
+    }
+    
+    
+},
+updated: function () {
+
+
+}
